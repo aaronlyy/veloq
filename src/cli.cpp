5fd73e00c53bdb3f4e722fe0e03a543cf10457f9
aaronlyy <aaronlevican@gmail.com>
2025-04-22 22:36:47 +0200

    refactor: added some flags and renamed other flags

--- src/cli.cpp
+++ src/cli.cpp
@@ -22,18 +22,23 @@ namespace veloq {
     bool live_output{false}; // print live output of commands
   
     // options
-    int runs_per_command{5}; // how often should the command be run
+    int runs_per_command{1}; // how often should the command be run
     std::filesystem::path output_path; // output path of replay file
     std::filesystem::path replay_path; // path to replay file that is going to be simulated
+    std::vector<std::string> config_path; // run benchmark using pre defined command
+    std::string filter_regex; // regex string that is used on every line to 
     std::vector<std::string> commands; // commands to be run
-    std::string name;
+    std::string name; // name of benchmark for save file
   
     app.add_flag("-d,--diff", show_diff, "Show diff of outputs"); // todo
     app.add_flag("-l,--live", live_output, "Show live output of commands");
-    app.set_version_flag("-v,--version", VERSION_INFO, "Shows version info");
-    app.add_option("-r,--runs", runs_per_command, "How often the commands should be executed. Default is 5.");
-    app.add_option("-s,--simulate", replay_path, "Path to replay file"); // todo
-    app.add_option("-o,--out", output_path, "Output path"); // todo
+
+    app.set_version_flag("--version", VERSION_INFO, "Shows version info");
+
+    app.add_option("-r,--runs", runs_per_command, "How often the commands should be executed. Default is 1.");
+    app.add_option("-f,--filter", filter_regex, "Filter output, prints and saves line if regex expression is true"); // TODO
+    app.add_option("-o,--out", output_path, "Output path"); // TODO: Path to save benchmark file
+    app.add_option("-c,--config", config_path, "Run benchmark using pre-defined config"); // >TODO: Path to benchmark config
     app.add_option("-n, --name", name, "Name of benchmark. Default is 'veloq benchmark'");
     app.add_option("commands", commands, "Commands to run")->expected(-1);
   
