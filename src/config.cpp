dc953902166b5db076760a586686c088453af863
aaronlyy <aaronlevican@gmail.com>
2025-04-23 19:56:48 +0200

    feat: added functions to load configs from file

---
+++ src/config.cpp
@@ -0,0 +1,13 @@
+#include "config.hpp"
+
+namespace veloq {
+  Config load_from_yaml() {
+    Config config;
+    return config;
+  }
+
+  Config load_from_json() {
+    Config config;
+    return config;
+  }
+}
