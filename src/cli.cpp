c9ecc7437ce97efc09f7cc277e857c5e865fbcd2
aaronlyy <aaronlevican@gmail.com>
2025-04-23 19:57:47 +0200

    refactor: removed benchmark stuff, added config builder

--- src/cli.cpp
+++ src/cli.cpp
@@ -8,63 +8,86 @@
 #include "CLI11.hpp"
 
 #include "cli.hpp"
-#include "runner.hpp"
-#include "utils.hpp"
-#include "benchmark.hpp"
-
+#include "config.hpp"
 namespace veloq {
   int handle_cli(int argc, char** argv) {
 
     CLI::App app{"veloq"};
 
+    // add version flag with build time
+    app.set_version_flag("--version", VERSION_INFO, "Shows version info");
+
+    std::vector<std::string> commands; // commands to be run
+
     // flags
+    bool record{false}; // record command output and add to savefile
     bool show_diff{false}; // show diffs of output
     bool live_output{false}; // print live output of commands
+    bool no_benchmark{false}; // hide benchmark ouput after command run
+    bool verbose{false}; // print debug info
   
     // options
     int runs_per_command{1}; // how often should the command be run
-    std::filesystem::path output_path; // output path of replay file
-    std::filesystem::path replay_path; // path to replay file that is going to be simulated
-    std::vector<std::string> config_path; // run benchmark using pre defined command
-    std::string filter_regex; // regex string that is used on every line to 
-    std::vector<std::string> commands; // commands to be run
     std::string name; // name of benchmark for save file
-  
-    app.add_flag("-d,--diff", show_diff, "Show diff of outputs"); // todo
-    app.add_flag("-l,--live", live_output, "Show live output of commands");
+    std::string filter_regex; // regex string that is used on every line to 
+    std::string highlight_regex; // regex string to add color to special lines
+    std::filesystem::path output_path; // output path for savefile
+    std::vector<std::string> config_path; // run veloq using pre defined config
 
-    app.set_version_flag("--version", VERSION_INFO, "Shows version info");
+    // cli flags
+    app.add_flag("-d,--diff", show_diff, "Show diff of outputs");
+    app.add_flag("-l,--live", live_output, "Show live output of commands");
+    app.add_flag("--no-benchmark", no_benchmark, "Hide benchmark result ouput in terminal after commands are finished running");
+    app.add_flag("-v,--verbose", verbose, "Print debug information");
+
+    // cli options
+    app.add_option("commands", commands, "Commands to run")->expected(-1);
 
     app.add_option("-r,--runs", runs_per_command, "How often the commands should be executed. Default is 1.");
     app.add_option("-f,--filter", filter_regex, "Filter output, prints and saves line if regex expression is true"); // TODO
+    app.add_option("-h,--highlight", highlight_regex, "Regex string to highlight command output");
     app.add_option("-o,--out", output_path, "Output path"); // TODO: Path to save benchmark file
     app.add_option("-c,--config", config_path, "Run benchmark using pre-defined config"); // >TODO: Path to benchmark config
     app.add_option("-n, --name", name, "Name of benchmark. Default is 'veloq benchmark'");
-    app.add_option("commands", commands, "Commands to run")->expected(-1);
-  
+    app.add_option("-c,--config", config_path, "Path to veloq config file");
+    
     CLI11_PARSE(app, argc, argv);
 
-    if (commands.size() == 0) {
-      std::cout << "huh? maybe try 'veloq --help'\n";
-      return 0;
-    }
-    else {
-      BenchmarkConfig benchmark_config;
-      benchmark_config.name = name;
-      benchmark_config.runs_per_command = runs_per_command;
-      benchmark_config.commands = commands;
-      benchmark_config.live_output = live_output;
-  
-      Benchmark benchmark(benchmark_config);
-      BenchmarkResult benchmark_result = benchmark.start();
+    // create config object for controller
+    Config config;
 
-      for (RunCollection &run_collection :  benchmark_result.run_collections) {
-        std::cout << "Command: " << run_collection.command << std::endl;
-        std::cout << "Full Runtime: " << run_collection.runtime() << "s" << std::endl;
-        std::cout << "Average runtime: " << run_collection.average_runtime() << "s" << std::endl;
-        std::cout << "Median runtime: " << run_collection.median_runtime() << "s" << std::endl;
-      }
+    // set given flags in config
+    config.show_diff = show_diff;
+    config.live_output = live_output;
+    config.no_benchmark = no_benchmark;
+    config.verbose = verbose;
+
+    // only set options if given
+    if (app.get_option("commands")->count()) {
+      config.commands = commands;
     }
+
+    if (app.get_option("--runs")->count()) {
+      config.runs_per_command = runs_per_command;
+    }
+    
+    if (app.get_option("--name")->count()) {
+      config.name = name;
+    }
+
+    if (app.get_option("--filter")->count()) {
+      config.filter_regex = filter_regex;
+    }
+
+    if (app.get_option("--highlight")->count()) {
+      config.highlight_regex = highlight_regex;
+    }
+
+    if (app.get_option("--out")->count()) {
+      config.output_path = output_path.string(); // optional<string>
+    }
+
+    // create controller object for handling the actual running of the awesome stuff
 
     return 0;
   }
