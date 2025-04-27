ef8bd7cfecb779b216b48c85e63e5a03e5b38b58
aaronlyy <aaronlevican@gmail.com>
2025-04-27 18:12:57 +0200

    fix: added virtual terminal enable for colors on windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "cli.hpp"
+#include "format/color.hpp"
 
 int main(int argc, char** argv) {
-  // just forward args to the cli, nothing else happens here
+  // set console mode on windows to be able to use ansi escape sequences
+  veloq::format::enable_virtual_terminal();
+
+  // parse args
   return veloq::handle_cli(argc, argv);
 }
